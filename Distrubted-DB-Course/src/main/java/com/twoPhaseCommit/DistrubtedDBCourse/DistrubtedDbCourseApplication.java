03900163a6830dcb8a31a4aa6a78dd1cff4b4a66
itaytas <itaytas@gmail.com>
2019-01-17 18:24:48 +0200

    Project set-up

---
+++ Distrubted-DB-Course/src/main/java/com/twoPhaseCommit/DistrubtedDBCourse/DistrubtedDbCourseApplication.java
@@ -0,0 +1,14 @@
+package com.twoPhaseCommit.DistrubtedDBCourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistrubtedDbCourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DistrubtedDbCourseApplication.class, args);
+	}
+
+}
+
